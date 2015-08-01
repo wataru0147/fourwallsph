9aa199b49cf9272524710e1c2ef3c5667983bc5f
wataru0147 <jerrome.pelacio@gmail.com>
2015-08-01 15:24:50 +0800

    Login and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,9 @@
 module SessionsHelper
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
   def log_in(user) #login method defined here is used on sessions controller
     session[:user_id] = user.id
   end
@@ -9,8 +14,9 @@ module SessionsHelper
     #if current user has id then store it
   end
 
-  # Returns true if the user is logged in, false otherwise.
-  def logged_in?
-    !current_user.nil?
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", signup_path
-    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", contactus_path
     assert_select "a[href=?]", search_path
 
